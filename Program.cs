e73528066b833b7678bb59c2b378a0ba4421b344
SamuellRodrigues <baurukigamer@gmail.com>
2025-01-30 16:51:16 -0300

    txt

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ string especiais;
 
 
 
+
 Console.WriteLine("----Gerador de Senha----");
 Console.Write("Digite a quantidede de caracteres da senha: ");
 
@@ -19,27 +20,47 @@ if (!int.TryParse(Console.ReadLine(), out int qtdSenha))
 Console.Write("Deseja incluir letras? (s/n):");
 letras = Console.ReadLine();
 
-if (letras.ToLower() == "s")
 
+
+
+Console.Write("Deseja incluir numeros? (s/n):");
+numero = Console.ReadLine();
+
+
+
+Console.Write("Deseja incluir caracteres especiais? (@, !, # ou -) (s/n):");
+especiais = Console.ReadLine();
+
+
+
+
+string permetidos = "";
+
+Random random = new Random(); // é utilizado o Random() para posibilitar gerar um valor aleatorio dentro de um intevalo da variavel random
+if (letras.ToLower() == "s")// letra uma variavel declarada é ToLower é para ler tudo ou converter demaiusculo para minúsculas
+  permetidos += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+if (numero.ToLower() == "s")
+  permetidos += "0123456789";
+
+if (especiais.ToLower() == "s")
+  permetidos += "@, !, # ou -";
+
+
+
+string gerar = new string(Enumerable.Repeat(permetidos, qtdSenha).Select(s => s[random.Next(s.Length)]).ToArray());
+Console.WriteLine($"Senha Gerada {gerar}");
+
+
+// string caminhoArquivo = "bkp.TXT";
+
+//  SalvarSenhaEmArquivo(gerar, caminhoArquivo);
+
+using (StreamWriter escrever = new StreamWriter("senha.txt"))
 {
-  Console.Write("Deseja incluir numeros? (s/n):");
-  numero = Console.ReadLine();
-
-  Console.Write("Deseja incluir caracteres especiais? (!@#$%&*) (s/n):");
-  especiais = Console.ReadLine();
-
-
-
+  Console.WriteLine("senha salva");  // Imprime na tela
+  escrever.WriteLine($"{gerar}"); // Salva no arquivo
 }
-
-int numeroaleatorio;
-Random random = new Random();
-for (int i = 0; i < qtdSenha; i++)
-{
-  numeroaleatorio =+ random.Next(0, 9);
-  Console.WriteLine($"senha {numeroaleatorio}");
-}
-
 
 
 
--- obj/Debug/net8.0/GeradorDeSenha.AssemblyInfo.cs
+++ obj/Debug/net8.0/GeradorDeSenha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeradorDeSenha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+773877472fcf860abbf4f8bd50eb1e5ea722093b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96381386660772d0eb3723ea830ea6c9702f8353")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeradorDeSenha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeradorDeSenha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
