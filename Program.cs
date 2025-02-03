c03f5370b32c190614f5e13bbac1241c46aed3b4
SamuellRodrigues <baurukigamer@gmail.com>
2025-02-03 15:18:00 -0300

    Final

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ string especiais;
 Console.WriteLine("----Gerador de Senha----");
 Console.Write("Digite a quantidede de caracteres da senha: ");
 
-if (!int.TryParse(Console.ReadLine(), out int qtdSenha))
+if (!int.TryParse(Console.ReadLine(), out int qtdSenha) || qtdSenha <= 0)
 {
   Console.WriteLine("valor invalido!");
   return;
@@ -34,34 +34,53 @@ especiais = Console.ReadLine();
 
 
 
-string permetidos = "";
+string permitidos = "";
 
 Random random = new Random(); // é utilizado o Random() para posibilitar gerar um valor aleatorio dentro de um intevalo da variavel random
 if (letras.ToLower() == "s")// letra uma variavel declarada é ToLower é para ler tudo ou converter demaiusculo para minúsculas
-  permetidos += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  permitidos += "ABCDEFGHIJKLMNOPQRSTUVMXYZÇ" + "ABCDEFGHIJKLMNOPQRSTUVMXYZÇ".ToLower();
 
 if (numero.ToLower() == "s")
-  permetidos += "0123456789";
+  permitidos += "0123456789";
 
 if (especiais.ToLower() == "s")
-  permetidos += "@, !, # ou -";
+  permitidos += "@!#-";
+
+  if (string.IsNullOrEmpty(permitidos))
+        {
+            Console.WriteLine("Nenhum tipo de caractere foi selecionado. Não é possível gerar uma senha.");
+            return;
+        }
 
 
 
-string gerar = new string(Enumerable.Repeat(permetidos, qtdSenha).Select(s => s[random.Next(s.Length)]).ToArray());
+string gerar = new string(Enumerable.Repeat(permitidos, qtdSenha).Select(s => s[random.Next(s.Length)]).ToArray());
 Console.WriteLine($"Senha Gerada {gerar}");
 
 
-// string caminhoArquivo = "bkp.TXT";
 
-//  SalvarSenhaEmArquivo(gerar, caminhoArquivo);
-
-using (StreamWriter escrever = new StreamWriter("senha.txt"))
+string caminhoArquivo = "senha.txt";
+using (StreamWriter escrever = new StreamWriter("senha.txt",true))
 {
-  Console.WriteLine("senha salva");  // Imprime na tela
-  escrever.WriteLine($"{gerar}"); // Salva no arquivo
+  Console.WriteLine("senha salva");  
+  escrever.WriteLine($"{gerar}"); 
 }
+if (File.Exists(caminhoArquivo))
+        {
+            Console.Write("Deseja recuperar senha(s/n):");
+            string resposta = Console.ReadLine()?.ToLower();
 
+            if (resposta == "s")
+            using (StreamReader ler = new StreamReader(caminhoArquivo))
+            {
+                string linha;
+                
+                while ((linha = ler.ReadLine()) != null)
+                {
+                    Console.WriteLine(linha);  
+                }
+            }
+        }
 
 
 
--- obj/Debug/net8.0/GeradorDeSenha.AssemblyInfo.cs
+++ obj/Debug/net8.0/GeradorDeSenha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeradorDeSenha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96381386660772d0eb3723ea830ea6c9702f8353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73528066b833b7678bb59c2b378a0ba4421b344")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeradorDeSenha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeradorDeSenha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
